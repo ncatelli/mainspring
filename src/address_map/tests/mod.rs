80e1efbaf7d0a2e5003d8679f44185227db9d67f
Nate Catelli <ncatelli@packetfire.org>
2020-12-31 04:19:29 +0000

    registration test

--- src/address_map/tests/mod.rs
+++ src/address_map/tests/mod.rs
@@ -1,1 +1,15 @@
+use crate::address_map::{
+    memory::{Memory, ReadOnly},
+    AddressMap,
+};
+
 mod memory;
+
+#[test]
+fn should_register_valid_memory() {
+    let addr_space = 0..std::u16::MAX;
+    let mem: Memory<ReadOnly> = Memory::new(addr_space.start, addr_space.end);
+    assert!(AddressMap::<u16>::new()
+        .register(addr_space, Box::new(mem))
+        .is_ok());
+}

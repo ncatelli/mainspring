b462152c4b814c9fb142368f057b7a1a55a2ead1
Nate Catelli <ncatelli@packetfire.org>
2020-12-31 02:26:16 +0000

    Addressable

--- src/address_map/mod.rs
+++ src/address_map/mod.rs
@@ -1,2 +1,11 @@
 #[cfg(tests)]
 mod tests;
+
+type WriteError = String;
+
+/// Addressable implements the trait for addressable memory in an address map.
+/// this can represent IO, RAM, ROM, etc...
+pub trait Addressable<O: Into<usize>> {
+    fn read(offset: O) -> u8;
+    fn write(offset: O, data: u8) -> Result<u8, WriteError>;
+}

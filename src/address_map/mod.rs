d05a4a6330215003d9e6b100800054b572ec6688
Nate Catelli <ncatelli@packetfire.org>
2021-01-01 00:29:50 +0000

    write methods

--- src/address_map/mod.rs
+++ src/address_map/mod.rs
@@ -11,14 +11,17 @@ type RegistrationError = String;
 
 /// Addressable implements the trait for addressable memory in an address map.
 /// this can represent IO, RAM, ROM, etc...
-pub trait Addressable<O: Into<usize>> {
+pub trait Addressable<O>
+where
+    O: Into<usize> + Debug,
+{
     fn read(&self, offset: O) -> u8;
     fn write(&mut self, offset: O, data: u8) -> Result<u8, WriteError>;
 }
 
 /// AddressMap
 pub struct AddressMap<O: Into<usize>> {
-    map: HashMap<Range<O>, Box<dyn Addressable<O>>>,
+    inner: HashMap<Range<O>, Box<dyn Addressable<O>>>,
 }
 
 impl<O> AddressMap<O>
@@ -27,7 +30,7 @@ where
 {
     pub fn new() -> Self {
         AddressMap {
-            map: HashMap::new(),
+            inner: HashMap::new(),
         }
     }
 
@@ -37,7 +40,7 @@ where
         range: Range<O>,
         addr_space: Box<dyn Addressable<O>>,
     ) -> Result<AddressMap<O>, RegistrationError> {
-        self.map
+        self.inner
             .keys()
             .map(|key| {
                 if key.contains(&range.start) || key.contains(&range.end) {
@@ -52,8 +55,40 @@ where
             .collect::<Result<Vec<()>, RegistrationError>>()
             .map_err(|e| e)
             .map(|_| {
-                self.map.insert(range, addr_space);
+                self.inner.insert(range, addr_space);
                 self
             })
+    }
+}
+
+impl<T> Addressable<T> for AddressMap<T>
+where
+    T: Into<usize> + Hash + PartialOrd + Eq + Debug + Copy,
+{
+    /// Reads a single byte at the specified address
+    fn read(&self, addr: T) -> u8 {
+        self.inner
+            .keys()
+            .filter(|key| key.contains(&addr))
+            .map(|r| self.inner.get(r))
+            .flatten()
+            .next()
+            .map_or(0x00, |a| a.read(addr))
+    }
+
+    /// Write assigns a single value to an address in memory
+    fn write(&mut self, addr: T, value: u8) -> Result<u8, String> {
+        let range = self
+            .inner
+            .keys()
+            .map(|k| k.clone())
+            .filter(|key| key.contains(&addr))
+            .next()
+            .ok_or(format!("address space {:?} unallocated", addr))?;
+        let am = self
+            .inner
+            .get_mut(&range)
+            .ok_or(format!("address space {:?} unallocated", addr))?;
+        am.write(addr, value)
     }
 }

c8e6ffef279938c922ecf132c9923e7a276f17fd
Nate Catelli <ncatelli@packetfire.org>
2021-01-02 19:26:53 +0000

    WIP

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -23,10 +23,16 @@ impl CPU {
         Self::default()
     }
 
+    /// instantiates a new cpu with a provided address_map.
     pub fn with_addressmap(am: AddressMap<u16>) -> Self {
         let mut cpu = Self::default();
         cpu.address_map = am;
         cpu
+    }
+
+    /// emulates the reset process of the CPU.
+    pub fn reset(self) -> Self {
+        CPU::with_addressmap(self.address_map)
     }
 }
 

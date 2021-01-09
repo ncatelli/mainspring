bc377c9d4e7e999e7ab1640c61f2bb7db6dc47e1
Nate Catelli <ncatelli@packetfire.org>
2021-01-09 20:57:45 +0000

    Switch to tuple structs

--- src/cpu/mos6502/operations.rs
+++ src/cpu/mos6502/operations.rs
@@ -1,89 +1,91 @@
-#[derive(Copy, Clone, PartialEq, Debug)]
-/// Operation takes a mnemonic
-pub struct Operation<M, A> {
-    mnemonic: M,
-    address_mode: A,
-}
+extern crate parcel;
+use parcel::{ParseResult, Parser};
 
 // Load-Store
-pub struct LDA {}
-pub struct LDX {}
-pub struct LDY {}
-pub struct STA {}
-pub struct STX {}
-pub struct STY {}
+pub struct LDA;
+pub struct LDX;
+pub struct LDY;
+pub struct STA;
+pub struct STX;
+pub struct STY;
 
 // Arithmetic
-pub struct ADC {}
-pub struct SBC {}
-pub struct INC {}
-pub struct INX {}
-pub struct INY {}
-pub struct DEC {}
-pub struct DEX {}
-pub struct DEY {}
+pub struct ADC;
+pub struct SBC;
+pub struct INC;
+pub struct INX;
+pub struct INY;
+pub struct DEC;
+pub struct DEX;
+pub struct DEY;
 
 // Shift and Rotate
-pub struct ASL {}
-pub struct LSR {}
-pub struct ROL {}
-pub struct ROR {}
-pub struct AND {}
-pub struct ORA {}
-pub struct EOR {}
+pub struct ASL;
+pub struct LSR;
+pub struct ROL;
+pub struct ROR;
+pub struct AND;
+pub struct ORA;
+pub struct EOR;
 
 // Compare and Test Bit
-pub struct CMP {}
-pub struct CPX {}
-pub struct CPY {}
-pub struct BIT {}
+pub struct CMP;
+pub struct CPX;
+pub struct CPY;
+pub struct BIT;
 
 // Branch
-pub struct BCC {}
-pub struct BCS {}
-pub struct BNE {}
-pub struct BEQ {}
-pub struct BPL {}
-pub struct BMI {}
-pub struct BVC {}
-pub struct BVS {}
+pub struct BCC;
+pub struct BCS;
+pub struct BNE;
+pub struct BEQ;
+pub struct BPL;
+pub struct BMI;
+pub struct BVC;
+pub struct BVS;
 
 // Transfer
-pub struct TAX {}
-pub struct TXA {}
-pub struct TAY {}
-pub struct TYA {}
-pub struct TSX {}
-pub struct TXS {}
+pub struct TAX;
+pub struct TXA;
+pub struct TAY;
+pub struct TYA;
+pub struct TSX;
+pub struct TXS;
 
 // Stack Operations
-pub struct PHA {}
-pub struct PLA {}
-pub struct PHP {}
-pub struct PLP {}
+pub struct PHA;
+pub struct PLA;
+pub struct PHP;
+pub struct PLP;
 
 // Subroutines and Jump
-pub struct JMP {}
-pub struct JSR {}
-pub struct RTS {}
-pub struct RTI {}
+pub struct JMP;
+pub struct JSR;
+pub struct RTS;
+pub struct RTI;
 
 // Set and Clear
-pub struct CLC {}
-pub struct SEC {}
-pub struct CLD {}
-pub struct SED {}
-pub struct CLI {}
-pub struct SEI {}
-pub struct CLV {}
+pub struct CLC;
+pub struct SEC;
+pub struct CLD;
+pub struct SED;
+pub struct CLI;
+pub struct SEI;
+pub struct CLV;
 
 // Misc
-pub struct BRK {}
-pub struct NOP {}
+pub struct BRK;
+pub struct NOP;
+
+impl<'a> Parser<'a, &'a [u8], NOP> for NOP {
+    fn parse(&self, _: &'a [u8]) -> ParseResult<&'a [u8], NOP> {
+        todo!()
+    }
+}
 
 /// Address modes
-pub struct Accumulator();
-pub struct Implied();
+pub struct Accumulator;
+pub struct Implied;
 pub struct Immediate(u8);
 pub struct Absolute(u16);
 pub struct ZeroPage(u8);
@@ -95,3 +97,10 @@ pub struct ZeroPageIndexedWithX(u8);
 pub struct ZeroPageIndexedWithY(u8);
 pub struct IndexedIndirect(u8);
 pub struct IndirectIndexed(u8);
+
+#[derive(Copy, Clone, PartialEq, Debug)]
+/// Operation takes a mnemonic
+pub struct Operation<M, A> {
+    mnemonic: M,
+    address_mode: A,
+}

82b1326aa97225e2abc5a00e59ceddab91fe0f80
Nate Catelli <ncatelli@packetfire.org>
2021-01-10 00:08:59 +0000

    Implement parser for implied am

--- src/cpu/mos6502/operations/mod.rs
+++ src/cpu/mos6502/operations/mod.rs
@@ -1,4 +1,6 @@
+extern crate parcel;
 use crate::cpu::{Cyclable, Offset};
+use parcel::{ParseResult, Parser};
 
 pub mod address_mode;
 pub mod mnemonic;
@@ -11,4 +13,31 @@ where
 {
     mnemonic: M,
     address_mode: A,
+}
+
+impl<M, A> Operation<M, A>
+where
+    M: Cyclable + Offset,
+    A: Cyclable + Offset,
+{
+    pub fn new(mnemonic: M, address_mode: A) -> Self {
+        Operation {
+            mnemonic,
+            address_mode,
+        }
+    }
+}
+
+impl<'a> Parser<'a, &'a [u8], Operation<mnemonic::NOP, address_mode::Implied>>
+    for Operation<mnemonic::NOP, address_mode::Implied>
+{
+    fn parse(
+        &self,
+        input: &'a [u8],
+    ) -> ParseResult<&'a [u8], Operation<mnemonic::NOP, address_mode::Implied>> {
+        mnemonic::NOP
+            .and_then(|_| address_mode::Implied)
+            .map(|am| Operation::new(mnemonic::NOP, am))
+            .parse(input)
+    }
 }

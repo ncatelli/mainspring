ad4367e5f04bc0eb0ba41bed80951612330f670d
Nate Catelli <ncatelli@packetfire.org>
2021-01-17 04:40:34 +0000

    Finish MC generation tests

--- src/cpu/mos6502/operations/tests/code_generation.rs
+++ src/cpu/mos6502/operations/tests/code_generation.rs
@@ -116,3 +116,65 @@ fn should_generate_absolute_address_mode_sta_machine_code() {
         Into::<Vec<Vec<Microcode>>>::into(mc)
     )
 }
+
+#[test]
+fn should_generate_absolute_address_mode_jmp_machine_code() {
+    let cpu = MOS6502::default();
+    let op: Operation = Instruction::new(mnemonic::JMP, address_mode::Absolute(0x0100)).into();
+    let mc = op.generate(&cpu);
+
+    // check Mops value is correct
+    assert_eq!(
+        MOps::new(
+            3,
+            3,
+            vec![Microcode::WritePCRegister(WritePCRegister(0x0100))]
+        ),
+        mc
+    );
+
+    // validate mops -> vector looks correct
+    assert_eq!(
+        vec![
+            vec![],
+            vec![],
+            vec![
+                Microcode::WritePCRegister(WritePCRegister(0x0100)),
+                Microcode::IncPCRegister(IncPCRegister(3))
+            ]
+        ],
+        Into::<Vec<Vec<Microcode>>>::into(mc)
+    )
+}
+
+#[test]
+fn should_generate_indirect_address_mode_jmp_machine_code() {
+    let cpu = MOS6502::default();
+    let op: Operation = Instruction::new(mnemonic::JMP, address_mode::Indirect(0x0100)).into();
+    let mc = op.generate(&cpu);
+
+    // check Mops value is correct
+    assert_eq!(
+        MOps::new(
+            3,
+            5,
+            vec![Microcode::WritePCRegister(WritePCRegister(0x0000))]
+        ),
+        mc
+    );
+
+    // validate mops -> vector looks correct
+    assert_eq!(
+        vec![
+            vec![],
+            vec![],
+            vec![],
+            vec![],
+            vec![
+                Microcode::WritePCRegister(WritePCRegister(0x0000)),
+                Microcode::IncPCRegister(IncPCRegister(3))
+            ]
+        ],
+        Into::<Vec<Vec<Microcode>>>::into(mc)
+    )
+}

bd5013f2b1bb7435b6bcd279dec93342a7913b3e
Nate Catelli <ncatelli@packetfire.org>
2021-01-24 21:02:05 +0000

    add custom peripherals example

---
+++ examples/custom_peripheral.rs
@@ -0,0 +1,84 @@
+extern crate mainspring;
+use mainspring::address_map::memory::{Memory, ReadOnly};
+use mainspring::cpu::mos6502::MOS6502;
+
+#[allow(unused)]
+use mainspring::prelude::v1::*;
+
+/// VIA functions as an analogy to the 6522 VIA chip. However, only a subset of
+/// functionality has be implemented for this demo. For the case of this example,
+/// all ports are configured in write mode with predefined addresses.
+#[derive(Clone)]
+pub struct VIA {
+    port_a_addr: u16, // address connected to enable port a CS
+    port_b_addr: u16, // address connected to enable port a CS
+    port_a_mask: u16,
+    port_b_mask: u16,
+    port_a: u8,
+    port_b: u8,
+}
+
+impl VIA {
+    pub fn new(base_addr: u16) -> Self {
+        VIA {
+            port_a_addr: base_addr,
+            port_b_addr: base_addr + 1,
+            port_a_mask: base_addr + 2,
+            port_b_mask: base_addr + 3,
+            port_a: 0,
+            port_b: 0,
+        }
+    }
+}
+
+impl Addressable<u16> for VIA {
+    // Read the value at an address. Since this device is never read a
+    // constant is returned to satisfy the trait.
+    fn read(&self, _: u16) -> u8 {
+        0
+    }
+
+    /// Write a value to a a specified address. If the address matches port a,
+    /// it will write the value to the console after storing it.
+    fn write(&mut self, addr: u16, value: u8) -> Result<u8, String> {
+        if addr == self.port_a_addr {
+            self.port_a = value;
+            println!("{:08b}", value);
+            Ok(value)
+        } else if addr == self.port_b_addr {
+            self.port_b = value;
+            Ok(value)
+        } else if (self.port_a_mask..=self.port_b_mask).contains(&addr) {
+            Ok(value)
+        } else {
+            Err(format!("unknown VIA write addrress: {}", addr))
+        }
+    }
+}
+
+fn main() {
+    // A small rom that loops over write 01010101 and 10101010 to port a where
+    // it is output. this will loop endlessly until stopped.
+    let rom = Memory::<ReadOnly>::new(0x7fea, 0x7fff).load(vec![
+        0xa9, 0xff, 0x8d, 0x02, 0x80, 0xa9, 0x55, 0x8d, 0x00, 0x80, 0xa9, 0xaa, 0x8d, 0x00, 0x80,
+        0x4c, 0xef, 0x7f, 0xea, 0x7f, 0x00, 0x00,
+    ]);
+
+    let via = VIA::new(0x8000);
+    let cpu = MOS6502::default()
+        // Registers the address space and the rom as addressable memory with
+        // the cpu. This accepts any implementation of the Addressable trait.
+        .register_address_space(0x7fea..=0x7fff, rom)
+        // Registration can fail, this unwraps the result.
+        .unwrap()
+        .register_address_space(0x8000..=0x8003, via)
+        .unwrap()
+        // Resets the cpu and loads the reset vector into the PC.
+        .reset()
+        // This return a StepState<MOS6502> which is unwrapped to return the
+        // enclosing cpu.
+        .unwrap();
+
+    // runs the program for 40 cycles
+    cpu.run(40).unwrap();
+}

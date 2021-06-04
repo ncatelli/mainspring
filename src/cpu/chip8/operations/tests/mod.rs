029991bc28cdd93f8a114b2b0554738a593156e5
Nate Catelli <ncatelli@packetfire.org>
2021-06-04 05:28:22 +0000

    Add or byte register operation

--- src/cpu/chip8/operations/tests/mod.rs
+++ src/cpu/chip8/operations/tests/mod.rs
@@ -250,3 +250,48 @@ fn should_generate_and_byte_register_operation() {
         .generate(&cpu)
     );
 }
+
+#[test]
+fn should_parse_or_byte_register_operation_opcode() {
+    let input: Vec<(usize, u8)> = 0x8011u16
+        .to_be_bytes()
+        .iter()
+        .copied()
+        .enumerate()
+        .collect();
+    assert_eq!(
+        Ok(MatchStatus::Match {
+            span: 0..2,
+            remainder: &input[2..],
+            inner: Or::new(addressing_mode::ByteRegisterOperation::new(
+                register::GpRegisters::V1,
+                register::GpRegisters::V0
+            ))
+        }),
+        <Or<addressing_mode::ByteRegisterOperation>>::default().parse(&input[..])
+    );
+}
+
+#[test]
+fn should_generate_or_byte_register_operation() {
+    let cpu = Chip8::default()
+        .with_gp_register(
+            register::GpRegisters::V0,
+            register::GeneralPurpose::<u8>::with_value(0xff),
+        )
+        .with_gp_register(
+            register::GpRegisters::V1,
+            register::GeneralPurpose::<u8>::with_value(0x0f),
+        );
+    assert_eq!(
+        vec![Microcode::Write8bitRegister(Write8bitRegister::new(
+            register::ByteRegisters::GpRegisters(register::GpRegisters::V0),
+            0xff
+        ))],
+        Or::new(addressing_mode::ByteRegisterOperation::new(
+            register::GpRegisters::V1,
+            register::GpRegisters::V0
+        ))
+        .generate(&cpu)
+    );
+}
